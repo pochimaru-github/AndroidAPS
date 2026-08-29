967e8cc508aedee589884ce16c9338a2344a9822
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-08-29 21:12:57 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.test) apply false
-    alias(libs.plugins.kotlin.android) apply false
-}
-
 buildscript {
     repositories {
         mavenCentral()
