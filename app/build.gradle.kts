a0bce1c2806848616219ec4560250f868ddc56f8
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-08-29 21:56:23 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
-apply(plugin = "com.android.application")
-apply(plugin = "org.jetbrains.kotlin.android")
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
 repositories {
     mavenCentral()
@@ -8,23 +10,21 @@ repositories {
 }
 
 android {
-    compileSdk(34)
+    compileSdk = 34
 
     defaultConfig {
-        applicationId("com.nightscout.androidaps")
-        minSdk(21)
-        targetSdk(34)
-        versionCode(3040206)
-        versionName("3.4.2.6")
+        applicationId = "com.nightscout.androidaps"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 3040206
+        versionName = "3.4.2.6"
     }
 
     compileOptions {
-        sourceCompatibility = org.gradle.api.JavaVersion.VERSION_11
-        targetCompatibility = org.gradle.api.JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
-}
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
         jvmTarget = "11"
     }
