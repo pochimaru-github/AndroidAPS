59e0af570eea63bc407c1ff40e834810272f7186
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-08-29 21:28:00 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,9 +22,11 @@ android {
         sourceCompatibility = org.gradle.api.JavaVersion.VERSION_11
         targetCompatibility = org.gradle.api.JavaVersion.VERSION_11
     }
+}
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
-        jvmTarget("11")
+        jvmTarget = "11"
     }
 }
 
