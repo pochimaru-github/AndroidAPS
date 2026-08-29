7bdf16a43dbfba6cacea14d0964205c5960f3718
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-08-29 19:04:57 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 buildscript {
     repositories {
         mavenCentral()
         google()
     }
     dependencies {
-        classpath(libs.com.android.tools.build)
-        classpath(libs.com.google.gms)
-        classpath(libs.com.google.firebase.gradle)
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-
-        classpath(libs.kotlin.gradlePlugin)
-        classpath(libs.kotlin.allopen)
-        classpath(libs.kotlin.serialization)
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("com.google.gms:google-services:4.3.15")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
     }
-}
-
-plugins {
-    alias(libs.plugins.klint)
-    alias(libs.plugins.moduleDependencyGraph)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.compose.compiler) apply false
-    id(libs.plugins.android.test.get().pluginId) apply false
-    id(libs.plugins.kotlin.android.get().pluginId) apply false
 }
 
 allprojects {
@@ -41,24 +24,11 @@ allprojects {
             freeCompilerArgs.add("-opt-in=kotlin.RequiresOptIn")
             freeCompilerArgs.add("-opt-in=kotlin.ExperimentalUnsignedTypes")
             freeCompilerArgs.add("-Xannotation-default-target=param-property")
-            freeCompilerArgs.add("-Xjvm-default=all") //Support @JvmDefault
-            jvmTarget.set(Versions.jvmTarget)
+            freeCompilerArgs.add("-Xjvm-default=all")
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
         }
     }
-    gradle.projectsEvaluated {
-        tasks.withType<JavaCompile> {
-            val compilerArgs = options.compilerArgs
-            compilerArgs.add("-Xlint:deprecation")
-            compilerArgs.add("-Xlint:unchecked")
-        }
-    }
-
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
-    apply(plugin = "jacoco")
 }
-
-// Setup all reports aggregation
-apply(from = "jacoco_aggregation.gradle.kts")
 
 tasks.register<Delete>("clean").configure {
     delete(rootProject.layout.buildDirectory)
