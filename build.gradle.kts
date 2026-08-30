fe3f5acd8e632875a1e7115915f2aad887d6a326
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-08-30 11:37:13 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    
+    // 【追加】個別フォルダ（app）側の記述と100%連動させるための、必須プラグインの待ち受け宣言
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.8.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20" apply false
 }
 
 buildscript {
@@ -15,10 +19,6 @@ buildscript {
         classpath("com.google.gms:google-services:4.3.15")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
-        
-        // 【追加】画面部品と通信システムを安全に裏側で合流させるための必須クラスパス
-        classpath("org.jetbrains.kotlin:kotlin-allopen:1.8.20")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.8.20")
     }
 }
 
