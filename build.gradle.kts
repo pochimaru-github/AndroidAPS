b2714e9edb5d6ff61d7e4e9b21ea4b8e4189f42f
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-08-30 12:04:26 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    
-    // 【解説】個別フォルダ側の古い標準名（kotlin-allopen等）と100%合致するように待ち受け名を完璧に直しました
     id("kotlin-allopen") version "1.8.20" apply false
     id("kotlin-serialization") version "1.8.20" apply false
 }
@@ -19,6 +17,15 @@ buildscript {
         classpath("com.google.gms:google-services:4.3.15")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
+    }
+}
+
+// 【超重要】coreやdataなど、すべてのフォルダのJavaバージョンをここで一括で強制的に11に統一する絶対命令
+subprojects {
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
     }
 }
 
