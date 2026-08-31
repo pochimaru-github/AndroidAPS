d531ac12602a1912fa661ae1263747cbd18c4bc2
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-08-31 22:13:52 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
@@ -16,12 +18,62 @@ buildscript {
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
         classpath("io.objectbox:objectbox-gradle-plugin:3.6.0")
-        
         classpath("org.jetbrains.kotlin:kotlin-allopen:1.8.20")
     }
 }
 
-// 【解説】一括管理ルールと衝突していた個別の「allprojects { repositories { ... } }」のブロックをきれいにスッキリ削除しました！
+// 共通の翻訳ターゲット設定
+subprojects {
+    repositories {
+        mavenCentral()
+        google()
+    }
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            freeCompilerArgs = freeCompilerArgs + listOf(
+                "-opt-in=kotlin.RequiresOptIn",
+                "-Xjvm-default=all"
+            )
+            jvmTarget = "11"
+        }
+    }
+}
+
+// 【超重要】合流してきた「core」部屋の設定を、Android標準ライブラリとして完璧に工場に登録しました！
+project(":core") {
+    apply(plugin = "com.android.library")
+    apply(plugin = "org.jetbrains.kotlin.android")
+    
+    configure<com.android.build.gradle.LibraryExtension> {
+        namespace = "com.nightscout.androidaps.core"
+        compileSdk = 34
+        defaultConfig {
+            minSdk = 21
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+    }
+}
+
+// 【超重要】同じく「api」部屋の設定も、Android標準ライブラリとして100%美しく調和させました！
+project(":api") {
+    apply(plugin = "com.android.library")
+    apply(plugin = "org.jetbrains.kotlin.android")
+    
+    configure<com.android.build.gradle.LibraryExtension> {
+        namespace = "com.nightscout.androidaps.api"
+        compileSdk = 34
+        defaultConfig {
+            minSdk = 21
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+    }
+}
 
 tasks.register<Delete>("clean").configure {
     delete(rootProject.layout.buildDirectory)
