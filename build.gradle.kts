b2a0415a71a69b138b527504f148ae18a9930d01
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-01 16:31:00 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,8 +85,12 @@ project(":aaps") {
         sourceSets {
             getByName("main") {
                 manifest.srcFile("../app/src/main/AndroidManifest.xml")
+                
+                // 【ここに書き足し！】主役の部屋の奥底に眠る本物のアイコンや文字データを完璧に合流させました！
+                res.srcDirs("src/main/res")
             }
         }
+
         
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_11
