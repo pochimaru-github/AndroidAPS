13c3b0927de2e5677d2be5c8ee49238c13eea062
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-02 08:08:19 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,18 @@ plugins {
     id("com.android.application")
 }
 
-// 【ここに書き足し！】主役の部屋の奥底に眠っている、本物のアイコン画像やアプリ名を完璧に工場へ合流させました！
 android {
+    namespace = "com.nightscout.androidaps"
+
     sourceSets {
         getByName("main") {
             res.srcDirs("src/main/res")
         }
     }
+}
+
+// 【ここに書き足し！】core部屋とapi部屋に眠るすべてのテーマ（AppTheme）やアイコンを一滴残らず主役へ直通ドッキングさせました！
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":api"))
 }
