71ce4cc0039899fd5d293835c550bf70931b9c25
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-01 21:31:32 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
-// 【正真正銘の最終大正解】余計なルール（プラグイン）を一切挟まず、純粋な中継の材料置き場に仕立て直しました！
-dependencies {
-    "implementation"(project(":core"))
-    "implementation"(project(":api"))
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
 }
