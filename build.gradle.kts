7399f868312bd48dea9f661a6f29cab29f340ec5
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-01 13:23:58 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,9 +85,7 @@ project(":aaps") {
         sourceSets {
             getByName("main") {
                 manifest.srcFile("../app/src/main/AndroidManifest.xml")
-
-                // 【ここに書き足し！】app部屋に置き去りにされていた本物のアイコンやスタイルデータを完璧に合流させました！
-                res.srcDirs("../app/src/main/res")
+                res.srcDirs("../app/src/main/res", "res")
             }
         }
  
