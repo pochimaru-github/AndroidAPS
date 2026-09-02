360eb926e030fca7f289e01ef7962022f575905e
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-02 13:15:59 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,1 @@
-plugins {
-    id("com.android.application")
-}
 
-// 【正真正銘の最終大正解】二重衝突を起こしていた個別の android { ... } ブロックをすべて大元のボスへ100%引き継ぎ、純粋な材料ドッキング通路として覚醒させました！
-dependencies {
-    "api"(project(":core"))
-    "api"(project(":api"))
-}
