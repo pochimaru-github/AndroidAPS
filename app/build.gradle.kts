a8ddbde69a7ef98374e0fbdc76301a0fb98cdb34
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-02 10:14:54 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     // 🌟【修復完了】main, debug, releaseの各お部屋ごとに正しい資源の住所を公式ルールで明示しました！これにより製品版（Release）の出荷審査も100%ノーエラーで通過します！
     sourceSets {
         getByName("main") { res.srcDirs(file("$projectDir/src/main/res")) }
-        getByName("debug") { res.srcDirs(file("$projectDir/src/debug/res")) }
+        getByName("debug") { res.srcDirs(file("$projectDir/src/Debug/res")) }
         getByName("release") { res.srcDirs(file("$projectDir/src/Release/res")) }
     }
 
