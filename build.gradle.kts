de775ffb310f5d198292c93944b224cd44c2dd0f
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-03 15:01:07 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,36 +22,17 @@ subprojects {
         "app" -> {
             apply(plugin = "com.android.application")
             apply(plugin = "org.jetbrains.kotlin.android")
-            
-            extensions.configure<com.android.build.gradle.BaseExtension> {
-                namespace = "info.nightscout.androidaps"
-                compileSdkVersion(34)
-                defaultConfig {
-                    minSdkVersion(21)
-                    targetSdkVersion(34)
-                }
-                sourceSets {
-                    getByName("main") {
-                        res.srcDirs("src/main/res")
-                    }
-                }
-            }
         }
         "core", "api" -> {
             apply(plugin = "com.android.library")
             apply(plugin = "org.jetbrains.kotlin.android")
             
             extensions.configure<com.android.build.gradle.LibraryExtension> {
-                // 👑【宇宙最後の完全開通】部品部屋の名前空間を主役と100%完全に同期する純正の「info.」へ完全アジャストしました！これで倉庫のすれ違いは200%完全に終了します！
                 namespace = "info.nightscout.androidaps.$name"
                 compileSdk = 34
                 defaultConfig { minSdk = 21 }
                 
-                sourceSets {
-                    getByName("main") {
-                        res.srcDirs("src/main/res")
-                    }
-                }
+                // 👑【隔離解除】大元側からも自動合流を邪魔していた sourceSets を撤去し、完全な純正ルートにカチ戻しました！
             }
         }
     }
