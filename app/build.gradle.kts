b3bff6719e2e528e49314a832213a55cdd8e8365
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-04 13:09:33 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,27 +1,18 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.kapt")
+    id("kotlin-android")
+    id("kotlin-kapt")
 }
 
 android {
-    namespace = "app.aaps"
-    compileSdk = 34
+    compileSdkVersion(34)
 
     defaultConfig {
-        applicationId = "info.nightscout.androidaps"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 3040206
-        versionName = "3.4.2.6"
-
-        buildConfigField("String", "VERSION", "\"$version\"")
-        buildConfigField("String", "BUILDVERSION", "\"${generateGitBuild()}-${generateDate()}\"")
-        buildConfigField("String", "REMOTE", "\"${generateGitRemote()}\"")
-        buildConfigField("String", "HEAD", "\"${generateGitBuild()}\"")
-        buildConfigField("String", "COMMITTED", "\"${allCommitted()}\"")
-
-        testInstrumentationRunner = "app.aaps.runners.InjectedTestRunner"
+        applicationId("com.nightscout.androidaps")
+        minSdkVersion(21)
+        targetSdkVersion(34)
+        versionCode(3040206)
+        versionName("3.4.2.6")
     }
 
     compileOptions {
@@ -31,6 +22,76 @@ android {
 
     kotlinOptions {
         jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+}
+
+val output = File.createTempFile("git-remote", "")
+processBuilder.redirectOutput(output)
+val process = processBuilder.start()
+process.waitFor()
+return output.readText().trim()
+} catch (_: Exception) {
+    return "NoGitSystemAvailable"
+}
+}
+
+fun generateDate(): String {
+    val stringBuilder: StringBuilder = StringBuilder()
+    // showing only date prevents app to rebuild everytime
+    stringBuilder.append(SimpleDateFormat("yyyy.MM.dd").format(Date()))
+    return stringBuilder.toString()
+}
+
+fun isMaster(): Boolean = !Versions.appVersion.contains("-")
+
+fun gitAvailable(): Boolean {
+    try {
+        val processBuilder = ProcessBuilder("git", "--version")
+        val output = File.createTempFile("git-version", "")
+        processBuilder.redirectOutput(output)
+        val process = processBuilder.start()
+        process.waitFor()
+        return output.readText().isNotEmpty()
+    } catch (_: Exception) {
+        return false
+    }
+}
+
+fun allCommitted(): Boolean {
+    try {
+        val processBuilder = ProcessBuilder("git", "status", "-s")
+        val output = File.createTempFile("git-comited", "")
+        processBuilder.redirectOutput(output)
+        val process = processBuilder.start()
+        process.waitFor()
+        return output.readText().replace(Regex("""(?m)^\s*(M|A|D|\?\?)\s*.*?\.idea\/codeStyles\/.*?\s*$"""), "")
+            // ignore all files added to project dir but not staged/known to GIT
+            .replace(Regex("""(?m)^\s*(\?\?)\s*.*?\s*$"""), "").trim().isEmpty()
+    } catch (_: Exception) {
+        return false
+    }
+}
+
+android {
+
+    namespace = "app.aaps"
+
+    defaultConfig {
+        minSdk = Versions.minSdk
+        targetSdk = Versions.targetSdk
+
+        buildConfigField("String", "VERSION", "\"$version\"")
+        buildConfigField("String", "BUILDVERSION", "\"${generateGitBuild()}-${generateDate()}\"")
+        buildConfigField("String", "REMOTE", "\"${generateGitRemote()}\"")
+        buildConfigField("String", "HEAD", "\"${generateGitBuild()}\"")
+        buildConfigField("String", "COMMITTED", "\"${allCommitted()}\"")
+
+        // For Dagger injected instrumentation tests in app module
+        testInstrumentationRunner = "app.aaps.runners.InjectedTestRunner"
     }
 
     flavorDimensions.add("standard")
@@ -72,16 +133,22 @@ android {
 
     useLibrary("org.apache.http.legacy")
 
+    //Deleting it causes a binding error
     buildFeatures {
         dataBinding = true
         buildConfig = true
     }
 }
 
-dependencies {
-    implementation(fileTree(dir = "libs", include = ["*.jar"]))
+allprojects {
+    repositories {
+    }
+}
 
-    // 👑【後半のエラー全潰し！】あなたが運んでくれた40個以上の全部品部屋との直結命令を、Kotlin形式の正しい implementation 文法へ一撃で根こそぎ完全修復しました！
+dependencies {
+    // in order to use internet"s versions you"d need to enable Jetifier again
+    // https://github.com
+    // https://github.com
     implementation(project(":shared:impl"))
     implementation(project(":core:data"))
     implementation(project(":core:objects"))
@@ -108,22 +175,22 @@ dependencies {
     implementation(project(":implementation"))
     implementation(project(":database:impl"))
     implementation(project(":database:persistence"))
-    implementation(project(":pump:combov2"))
-    implementation(project(":pump:dana"))
-    implementation(project(":pump:danars"))
-    implementation(project(":pump:danar"))
-    implementation(project(":pump:diaconn"))
-    implementation(project(":pump:eopatch"))
-    implementation(project(":pump:medtrum"))
-    implementation(project(":pump:equil"))
-    implementation(project(":pump:insight"))
-    implementation(project(":pump:medtronic"))
-    implementation(project(":pump:common"))
-    implementation(project(":pump:omnipod:common"))
-    implementation(project(":pump:omnipod:eros"))
-    implementation(project(":pump:omnipod:dash"))
-    implementation(project(":pump:rileylink"))
-    implementation(project(":pump:virtual"))
+    include ':pump:combov2'
+    include ':pump:dana'
+    include ':pump:danars'
+    include ':pump:danar'
+    include ':pump:diaconn'
+    include ':pump:eopatch'
+    include ':pump:medtrum'
+    include ':pump:equil'
+    include ':pump:insight'
+    include ':pump:medtronic'
+    include ':pump:common'
+    include ':pump:omnipod:common'
+    include ':pump:omnipod:eros'
+    include ':pump:omnipod:dash'
+    include ':pump:rileylink'
+    include ':pump:virtual'
     implementation(project(":workflow"))
 
     testImplementation(project(":shared:tests"))
@@ -133,11 +200,18 @@ dependencies {
 
     debugImplementation(libs.com.squareup.leakcanary.android)
 
+
     kspAndroidTest(libs.com.google.dagger.android.processor)
+
+    /* Dagger2 - We are going to use dagger.android which includes
+     * support for Activity and fragment injection so we need to include
+     * the following dependencies */
     ksp(libs.com.google.dagger.android.processor)
     ksp(libs.com.google.dagger.compiler)
 
+    // MainApp
     api(libs.com.uber.rxdogtag2.rxdogtag)
+    // Remote config
     api(libs.com.google.firebase.config)
 }
 
@@ -146,72 +220,9 @@ println("isMaster: ${isMaster()}")
 println("gitAvailable: ${gitAvailable()}")
 println("allCommitted: ${allCommitted()}")
 println("-------------------")
-
 if (!gitAvailable()) {
     throw GradleException("GIT system is not available. On Windows try to run Android Studio as an Administrator. Check if GIT is installed and Studio have permissions to use it")
 }
 if (isMaster() && !allCommitted()) {
     throw GradleException("There are uncommitted changes. Clone sources again as described in wiki and do not allow gradle update")
-}
-
-// 👑 以下の5つの関数（あなたが中盤で命がけでサルベージしてくれたGit・日付計算システム）も、1文字の漏れもなく完全修復形態で搭載済みだぜぃ！😎
-fun generateGitBuild(): String {
-    try {
-        val processBuilder = ProcessBuilder("git", "describe", "--always", "--abbrev=7")
-        val output = File.createTempFile("git-build", "")
-        processBuilder.redirectOutput(output)
-        val process = processBuilder.start()
-        process.waitFor()
-        return output.readText().trim()
-    } catch (_: Exception) {
-        return "NoGitSystemAvailable"
-    }
-}
-
-fun generateGitRemote(): String {
-    try {
-        val processBuilder = ProcessBuilder("git", "config", "--get", "remote.origin.url")
-        val output = File.createTempFile("git-remote", "")
-        processBuilder.redirectOutput(output)
-        val process = processBuilder.start()
-        process.waitFor()
-        return output.readText().trim()
-    } catch (_: Exception) {
-        return "NoGitSystemAvailable"
-    }
-}
-
-fun generateDate(): String {
-    val stringBuilder: StringBuilder = StringBuilder()
-    stringBuilder.append(SimpleDateFormat("yyyy.MM.dd").format(Date()))
-    return stringBuilder.toString()
-}
-
-fun isMaster(): Boolean = !Versions.appVersion.contains("-")
-
-fun gitAvailable(): Boolean {
-    try {
-        val processBuilder = ProcessBuilder("git", "--version")
-        val output = File.createTempFile("git-version", "")
-        processBuilder.redirectOutput(output)
-        val process = processBuilder.start()
-        process.waitFor()
-        return output.readText().isNotEmpty()
-    } catch (_: Exception) {
-        return false
-    }
-}
-
-fun allCommitted(): Boolean {
-    try {
-        val processBuilder = ProcessBuilder("git", "status", "-s")
-        val output = File.createTempFile("git-comited", "")
-        processBuilder.redirectOutput(output)
-        val process = processBuilder.start()
-        process.waitFor()
-        return output.readText().replace(Regex("""(?m)^\s*(M|A|D|\?\?)\s*.*?\.idea\/codeStyles\/.*?\s*$"""), "")
-            .replace(Regex("""(?m)^\s*(\?\?)\s*.*?\s*$"""), "").trim().isEmpty()
-    } catch (_: Exception) {
-        return false
-    }
 }
