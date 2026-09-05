114f81fb9a5bd7c05de76c8a2777d2b4faad15b3
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-05 16:19:20 +0900

    Update build.gradle.kts

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
@@ -6,4 +8,14 @@ plugins {
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "11"
+        freeCompilerArgs = freeCompilerArgs + listOf(
+            "-opt-in=kotlin.ExperimentalStdlibApi",
+            "-language-version=1.9"
+        )
+    }
 }
