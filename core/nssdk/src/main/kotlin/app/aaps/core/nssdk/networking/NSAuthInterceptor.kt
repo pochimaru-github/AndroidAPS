8bd04bb8882eb612677ecc82fbb924aafd8d63e3
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-06 13:07:16 +0900

    Update NSAuthInterceptor.kt

--- core/nssdk/src/main/kotlin/app/aaps/core/nssdk/networking/NSAuthInterceptor.kt
+++ core/nssdk/src/main/kotlin/app/aaps/core/nssdk/networking/NSAuthInterceptor.kt
@@ -58,10 +58,9 @@ internal class NSAuthInterceptor(private val refreshToken: String, private val r
             }
         }
     }
-
     private fun testCanRefresh(initialResponse: Response) {
         // Todo: use proper reason code once it is supplied by remote
-        if (initialResponse.body.string().contains(MESSAGE_DATE_HEADER_OUT_OF_TOLERANCE)) {
+        if (initialResponse.body?.string()?.contains(MESSAGE_DATE_HEADER_OUT_OF_TOLERANCE) == true) {
             throw DateHeaderOutOfToleranceException("Data header out of tolerance")
         }
     }
