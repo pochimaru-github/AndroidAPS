d2169b3f3dcc151d970f586a16e8ae35b2dc1009
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-06 12:24:04 +0900

    Update build.gradle.kts

--- core/graphview/build.gradle.kts
+++ core/graphview/build.gradle.kts
@@ -5,8 +5,18 @@ plugins {
 }
 
 android {
-    namespace = "app.aaps.core.graphview" // 既存の namespace 定義があればそれを優先
-    compileSdk = 34 // ← 追加
+    namespace = "com.jjoe64.graphview" // 【変更】Javaコードの import/パッケージ名と一致させる
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    // 【追加】Release ビルド時の R クラス生成を有効化
+    buildFeatures {
+        buildConfig = true
+        resValues = true
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
