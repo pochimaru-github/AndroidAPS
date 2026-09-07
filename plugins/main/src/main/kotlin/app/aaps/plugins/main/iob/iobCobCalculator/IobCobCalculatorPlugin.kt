08f8a2d976a090a3b3802b22347f5f233ab79497
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-07 17:01:24 +0900

    Update IobCobCalculatorPlugin.kt

--- plugins/main/src/main/kotlin/app/aaps/plugins/main/iob/iobCobCalculator/IobCobCalculatorPlugin.kt
+++ plugins/main/src/main/kotlin/app/aaps/plugins/main/iob/iobCobCalculator/IobCobCalculatorPlugin.kt
@@ -329,7 +329,7 @@ class IobCobCalculatorPlugin @Inject constructor(
         }
         return ads.getLastAutosensData(reason, aapsLogger, dateUtil)
     }
-    
+
     override fun getCobInfo(reason: String): CobInfo {
         val autosensData = ads.getLastAutosensData(reason, aapsLogger, dateUtil)
         var displayCob: Double? = null
@@ -340,15 +340,16 @@ class IobCobCalculatorPlugin @Inject constructor(
         if (autosensData != null) {
             displayCob = autosensData.cob
             var currentCob = displayCob
-            carbs.forEach { carb ->
-                if (carb.timestamp > autosensData.time && carb.timestamp <= now) {
-                    if (currentCob != null) {
-                        currentCob += carb.amount
-                        currentCob = max(currentCob, 0.0)
+            if (currentCob != null) {
+                var safeCob = currentCob
+                carbs.forEach { carb ->
+                    if (carb.timestamp > autosensData.time && carb.timestamp <= now) {
+                        safeCob += carb.amount
+                        safeCob = max(safeCob, 0.0)
                     }
                 }
+                displayCob = safeCob
             }
-            displayCob = currentCob
             timestamp = autosensData.time
         }
         // Future carbs
