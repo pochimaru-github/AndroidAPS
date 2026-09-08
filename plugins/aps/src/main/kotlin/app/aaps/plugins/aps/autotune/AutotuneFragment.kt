8631ac3e11e9f85d8a10ddb9053f16b27a6569b3
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-08 14:48:15 +0900

    Update AutotuneFragment.kt

--- plugins/aps/src/main/kotlin/app/aaps/plugins/aps/autotune/AutotuneFragment.kt
+++ plugins/aps/src/main/kotlin/app/aaps/plugins/aps/autotune/AutotuneFragment.kt
@@ -1,19 +1,18 @@
 package info.nightscout.androidaps.plugins.openaps.autotune
 
+import android.content.DialogInterface
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.fragment.app.Fragment
 import info.nightscout.androidaps.R
-import info.nightscout.androidaps.interfaces.PluginFragment
+import info.nightscout.androidaps.databinding.AutotuneFragmentBinding
+import info.nightscout.androidaps.interfaces.FragmentWithMenu
 import info.nightscout.androidaps.plugins.openaps.autotune.data.Autotune
 import info.nightscout.androidaps.plugins.openaps.autotune.data.AutotuneResult
+import info.nightscout.androidaps.utils.OKDialog
+import info.nightscout.androidaps.utils.SP
+import info.nightscout.androidaps.utils.SafeParse
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -21,37 +20,40 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.slf me.LoggerFactory
 
-class AutotuneFragment : Fragment(), PluginFragment {
+class AutotuneFragment : FragmentWithMenu() {
 
     private val log = LoggerFactory.getLogger(AutotuneFragment::class.java)
     private val scope = CoroutineScope(Dispatchers.Main + Job())
 
-    private lateinit var daysEditText: EditText
-    private lateinit var runButton: Button
-    private lateinit var resultTextView: TextView
+    private var _binding: AutotuneFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.autotune_fragment, container, false)
+    ): View {
+        _binding = AutotuneFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
 
-        daysEditText = view.findViewById(R.id.autotune_days)
-        runButton = view.findViewById(R.id.autotune_run_button)
-        resultTextView = view.findViewById(R.id.autotune_results)
-
-        runButton.setOnClickListener {
-            runAutotune()
+        binding.autotuneRunButton.setOnClickListener {
+            context?.let { ctx ->
+                OKDialog.showConfirmation(
+                    ctx,
+                    getString(R.string.autotune_confirm_title),
+                    getString(R.string.autotune_confirm_message),
+                    DialogInterface.OnClickListener { _, _ -> runAutotune() }
+                )
+            }
         }
 
         return view
     }
 
     private fun runAutotune() {
-        val days = daysEditText.text.toString().toIntOrNull() ?: 7
-        runButton.isEnabled = false
-        resultTextView.text = getString(R.string.autotune_running)
+        val days = SafeParse.parseInt(binding.autotuneDays.text.toString(), 7)
+        binding.autotuneRunButton.isEnabled = false
+        binding.autotuneResults.text = getString(R.string.autotune_running)
 
         scope.launch(Dispatchers.IO) {
             try {
@@ -59,17 +61,22 @@ class AutotuneFragment : Fragment(), PluginFragment {
                 val result: AutotuneResult = autotune.calculate(days)
 
                 withContext(Dispatchers.Main) {
-                    resultTextView.text = result.toFormattedString()
-                    runButton.isEnabled = true
+                    binding.autotuneResults.text = result.toFormattedString()
+                    binding.autotuneRunButton.isEnabled = true
                 }
             } catch (e: Exception) {
                 log.error("Error executing Autotune: ", e)
                 withContext(Dispatchers.Main) {
-                    resultTextView.text = getString(R.string.autotune_error, e.localizedMessage)
-                    runButton.isEnabled = true
+                    binding.autotuneResults.text = getString(R.string.autotune_error, e.localizedMessage)
+                    binding.autotuneRunButton.isEnabled = true
                 }
             }
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun getPluginName(): String = "Autotune"
