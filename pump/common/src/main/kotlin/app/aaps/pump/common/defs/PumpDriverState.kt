1c1193858cc40b2b049af143a39e499bd7f25718
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-08 10:08:20 +0900

    Update PumpDriverState.kt

--- pump/common/src/main/kotlin/app/aaps/pump/common/defs/PumpDriverState.kt
+++ pump/common/src/main/kotlin/app/aaps/pump/common/defs/PumpDriverState.kt
@@ -1,6 +1,8 @@
 package app.aaps.pump.common.defs
 
 import app.aaps.pump.common.R
+import app.aaps.core.ui.R as CoreUiR
+import app.aaps.core.interfaces.R as CoreIfR
 
 // TODO there are 3 classes now, that do similar things, sort of, need to define exact rules: PumpDeviceState, PumpDriverState, PumpStatusState
 
@@ -8,16 +10,16 @@ import app.aaps.pump.common.R
 enum class PumpDriverState(val resourceId: Int) {
 
     NotInitialized(R.string.pump_status_not_initialized), // this state should be set only when driver is created
-    Connecting(app.aaps.core.ui.R.string.connecting),
-    Connected(app.aaps.core.interfaces.R.string.connected),
+    Connecting(CoreUiR.string.connecting),
+    Connected(CoreIfR.string.connected),
     Initialized(R.string.pump_status_initialized), // this is weird state that probably won't be used, since its more driver centric that communication centric
     EncryptCommunication(R.string.pump_status_encrypt),
     Ready(R.string.pump_status_ready),
     Busy(R.string.pump_status_busy),
     Suspended(R.string.pump_status_suspended),
     ExecutingCommand(R.string.pump_status_executing_command),
-    Disconnecting(app.aaps.core.interfaces.R.string.disconnecting),
-    Disconnected(app.aaps.core.ui.R.string.disconnected);
+    Disconnecting(CoreIfR.string.disconnecting),
+    Disconnected(CoreUiR.string.disconnected);
 
     fun isConnected(): Boolean = this == Connected || this == Initialized || this == Busy || this == Suspended
     fun isInitialized(): Boolean = this == Initialized || this == Busy || this == Suspended
