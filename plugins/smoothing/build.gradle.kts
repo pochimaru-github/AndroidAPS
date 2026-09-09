590f9a963b62721ba1d164a108cd430cd291f55e
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-09 20:24:47 +0900

    Update build.gradle.kts

--- plugins/smoothing/build.gradle.kts
+++ plugins/smoothing/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.ksp)
     id("kotlin-android")
+    id("kotlin-kapt")
     id("android-module-dependencies")
     id("test-module-dependencies")
     id("jacoco-module-dependencies")
@@ -9,14 +9,31 @@ plugins {
 
 android {
     namespace = "app.aaps.plugins.smoothing"
-}
 
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
 
 dependencies {
     implementation(project(":core:data"))
     implementation(project(":core:interfaces"))
     implementation(project(":core:ui"))
 
-    ksp(libs.com.google.dagger.compiler)
-    ksp(libs.com.google.dagger.android.processor)
+    // Dagger (Kapt 構成)
+    implementation("com.google.dagger:dagger:2.50")
+    implementation("com.google.dagger:dagger-android:2.50")
+    implementation("com.google.dagger:dagger-android-support:2.50")
+    kapt("com.google.dagger:dagger-compiler:2.50")
+    kapt("com.google.dagger:dagger-android-processor:2.50")
+}
+
+kapt {
+    correctErrorTypes = true
 }
