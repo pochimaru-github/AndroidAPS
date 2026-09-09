ea70bb5d2bfd9d086106b56acc970a56751169ac
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-10 08:11:38 +0900

    Update build.gradle.kts

--- plugins/sync/build.gradle.kts
+++ plugins/sync/build.gradle.kts
@@ -9,14 +9,16 @@ android {
 
     buildFeatures {
         viewBinding = true
-        dataBinding = true
+        dataBinding = false
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+}
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
         jvmTarget = "17"
     }
