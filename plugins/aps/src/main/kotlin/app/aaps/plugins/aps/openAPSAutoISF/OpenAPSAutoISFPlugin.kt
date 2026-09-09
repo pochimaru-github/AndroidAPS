3958c17aa46066798fb65f6cbb6a48ca4e70739a
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-09 15:14:19 +0900

    Update OpenAPSAutoISFPlugin.kt

--- plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAutoISF/OpenAPSAutoISFPlugin.kt
+++ plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAutoISF/OpenAPSAutoISFPlugin.kt
@@ -444,9 +444,9 @@ open class OpenAPSAutoISFPlugin @Inject constructor(
         )
 
         val determineBasalResult = apsResultProvider.get()
-        determineBasalResult.rate = result.rate
-        determineBasalResult.duration = result.duration
-        determineBasalResult.reason = result.reason
+        determineBasalResult.rate(result.rate)
+        determineBasalResult.duration(result.duration)
+        determineBasalResult.reason(result.reason)
 
         // Preserve input data
         determineBasalResult.inputConstraints = inputConstraints
