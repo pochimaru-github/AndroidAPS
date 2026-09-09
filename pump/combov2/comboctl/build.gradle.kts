e2ed8d018e3e66c487bf59971c8528241f23309e
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-10 07:40:52 +0900

    Update build.gradle.kts

--- pump/combov2/comboctl/build.gradle.kts
+++ pump/combov2/comboctl/build.gradle.kts
@@ -22,7 +22,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+}
 
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
         jvmTarget = "17"
         freeCompilerArgs += listOf(
