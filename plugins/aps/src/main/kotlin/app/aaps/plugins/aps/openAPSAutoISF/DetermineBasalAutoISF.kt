718e019439c5007e370adcd2617419331c203992
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-09 13:09:55 +0900

    Update DetermineBasalAutoISF.kt

--- plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAutoISF/DetermineBasalAutoISF.kt
+++ plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAutoISF/DetermineBasalAutoISF.kt
@@ -1,19 +1,10 @@
 package app.aaps.plugins.aps.openAPSAutoISF
 
-import android.text.Spanned
-import app.aaps.core.data.model.GV
-import app.aaps.core.interfaces.aps.APSResult
 import app.aaps.core.interfaces.aps.AutosensResult
 import app.aaps.core.interfaces.aps.CurrentTemp
 import app.aaps.core.interfaces.aps.GlucoseStatus
-import app.aaps.core.interfaces.aps.IobTotal
 import app.aaps.core.interfaces.aps.MealData
-import app.aaps.core.interfaces.aps.OapsProfile
-import app.aaps.core.interfaces.aps.OapsProfileAutoIsf
-import app.aaps.core.interfaces.aps.Predictions
-import app.aaps.core.interfaces.constraints.Constraint
 import app.aaps.core.interfaces.profile.Profile
-import org.json.JSONObject
 import org.slf4j.LoggerFactory
 import kotlin.math.max
 import kotlin.math.min
@@ -22,8 +13,7 @@ class DetermineBasalAutoISF {
 
     private val log = LoggerFactory.getLogger(DetermineBasalAutoISF::class.java)
 
-    @Suppress("UNCHECKED_CAST")
-    fun <RT : APSResult> determineBasal(
+    fun determineBasal(
         glucoseStatus: GlucoseStatus,
         currentTemp: CurrentTemp,
         iobData: Any?,
@@ -32,7 +22,7 @@ class DetermineBasalAutoISF {
         mealData: MealData,
         microBolusAllowed: Boolean,
         reservoirData: Double?
-    ): RT {
+    ): AutoISFResult {
 
         val targetBg = profile.getTargetMgdl()
         val currentGlucose = glucoseStatus.glucose
@@ -59,67 +49,16 @@ class DetermineBasalAutoISF {
         val maxBasal = profile.getMaxDailyBasal()
         val calculatedRate = max(0.0, min(requiredBasalRate, maxBasal))
 
-        val resultInstance = AutoISFAPSResult<RT>().apply {
-            algorithm = APSResult.Algorithm.AUTO_ISF
-            rate = calculatedRate
-            duration = 30
+        return AutoISFResult(
+            rate = calculatedRate,
+            duration = 30,
             reason = "AutoISF Active (Ratio: %.2f, Adj ISF: %.1f)".format(dynamicRatio, adjustedIsf)
-            autosensResult = autosensData
-            this.currentTemp = currentTemp
-            this.glucoseStatus = glucoseStatus
-            this.mealData = mealData
-            carbsReq = 0
-            carbsReqWithin = 0
-        }
-
-        return resultInstance as RT
+        )
     }
 
-    private class AutoISFAPSResult<RT : APSResult> : APSResult {
-        override fun with(result: RT): APSResult = this
-
-        override var date: Long = System.currentTimeMillis()
-        override var reason: String = ""
-        override var rate: Double = 0.0
-        override var percent: Int = 0
-        override var duration: Int = 0
-        override var smb: Double = 0.0
-        override var usePercent: Boolean = false
-        override var carbsReq: Int = 0
-        override var carbsReqWithin: Int = 0
-        override var deliverAt: Long = 0
-        override var targetBG: Double = 0.0
-        override var hasPredictions: Boolean = false
-        override var variableSens: Double? = null
-        override var isfMgdlForCarbs: Double? = null
-        override var scriptDebug: List<String>? = null
-
-        override val predictionsAsGv: MutableList<GV> = mutableListOf()
-        override val latestPredictionsTime: Long = 0
-        override val isChangeRequested: Boolean = false
-        override var isTempBasalRequested: Boolean = false
-
-        override val carbsRequiredText: String get() = if (carbsReq > 0) "${carbsReq}g" else ""
-
-        override var inputConstraints: Constraint<Double>? = null
-        override var rateConstraint: Constraint<Double>? = null
-        override var percentConstraint: Constraint<Int>? = null
-        override var smbConstraint: Constraint<Double>? = null
-
-        override var algorithm: APSResult.Algorithm = APSResult.Algorithm.AUTO_ISF
-        override var autosensResult: AutosensResult? = null
-        override var iobData: Array<IobTotal>? = null
-        override var glucoseStatus: GlucoseStatus? = null
-        override var currentTemp: CurrentTemp? = null
-        override var oapsProfile: OapsProfile? = null
-        override var oapsProfileAutoIsf: OapsProfileAutoIsf? = null
-        override var mealData: MealData? = null
-
-        override fun resultAsString(): String = reason
-        override fun resultAsSpanned(): Spanned = android.text.SpannedString(reason)
-        override fun newAndClone(): APSResult = this
-        override fun json(): JSONObject? = null
-        override fun predictions(): Predictions? = null
-        override fun rawData(): Any = reason
-    }
+    data class AutoISFResult(
+        val rate: Double = 0.0,
+        val duration: Int = 0,
+        val reason: String = ""
+    )
 }
