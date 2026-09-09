45be78a93f22313715d94d03f8fc4af6471212d2
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-09 17:52:51 +0900

    Update build.gradle.kts

--- plugins/automation/build.gradle.kts
+++ plugins/automation/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.ksp)
     id("kotlin-android")
-    id("kotlin-kapt")
     id("android-module-dependencies")
     id("test-module-dependencies")
     id("jacoco-module-dependencies")
@@ -9,6 +9,11 @@ plugins {
 
 android {
     namespace = "app.aaps.plugins.automation"
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
 }
 
 dependencies {
@@ -34,15 +39,6 @@ dependencies {
     api(libs.com.github.rtchagas.pingplacepicker)
     api(libs.com.google.firebase.config)
 
-    // Dagger (Kapt 構成へ変更)
-    implementation("com.google.dagger:dagger:2.50")
-    implementation("com.google.dagger:dagger-android:2.50")
-    implementation("com.google.dagger:dagger-android-support:2.50")
-    kapt("com.google.dagger:dagger-compiler:2.50")
-    kapt("com.google.dagger:dagger-android-processor:2.50")
-}
-
-kapt {
-    correctErrorTypes = true
-    keepJavacAnnotationProcessors = true
+    ksp(libs.com.google.dagger.compiler)
+    ksp(libs.com.google.dagger.android.processor)
 }
