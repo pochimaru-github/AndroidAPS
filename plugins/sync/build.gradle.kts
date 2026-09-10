c6d883fd1a1e7cdd8b181abaf48e5c6181649d06
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-10 15:57:06 +0900

    Update build.gradle.kts

--- plugins/sync/build.gradle.kts
+++ plugins/sync/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.kotlin.kapt)
-    alias(libs.plugins.hilt.android)
+    id("com.android.library")
+    kotlin("multiplatform")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 kotlin {
