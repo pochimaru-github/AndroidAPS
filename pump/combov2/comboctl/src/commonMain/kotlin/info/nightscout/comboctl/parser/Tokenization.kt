cf45c01d3fef12589d6ccf8979e6a6a80eefb095
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-10 09:25:53 +0900

    Update Tokenization.kt

--- pump/combov2/comboctl/src/commonMain/kotlin/info/nightscout/comboctl/parser/Tokenization.kt
+++ pump/combov2/comboctl/src/commonMain/kotlin/info/nightscout/comboctl/parser/Tokenization.kt
@@ -6,6 +6,18 @@ import info.nightscout.comboctl.base.DisplayFrame
 import kotlin.math.sign
 
 /**
+ * Extension function on DisplayFrame to safely retrieve a pixel value at specified coordinates.
+ */
+fun DisplayFrame.getPixelAt(x: Int, y: Int): Boolean {
+    // If pixels list exists inside DisplayFrame, get value safely; otherwise return default
+    if (x < 0 || x >= DISPLAY_FRAME_WIDTH || y < 0 || y >= DISPLAY_FRAME_HEIGHT) {
+        return false
+    }
+    val index = y * DISPLAY_FRAME_WIDTH + x
+    return if (index < this.pixels.size) this.pixels[index] else false
+}
+
+/**
  * Structure containing details about a match discovered in a [DisplayFrame].
  *
  * The match is referred to as a "token", similar to lexical tokens
@@ -33,7 +45,7 @@ typealias Tokens = List<Token>
 /**
  * Checks if the region at the given coordinates matches the given pattern.
  *
- * This is used for finding tokerns in a frame.
+ * This is used for finding tokens in a frame.
  *
  * @param displayFrame [DisplayFrame] that contains the region to match the pattern against.
  * @param pattern Pattern to match with the region in the display frame.
@@ -48,9 +60,6 @@ fun checkIfPatternMatchesAt(displayFrame: DisplayFrame, pattern: Pattern, x: Int
         ((x + pattern.width) > DISPLAY_FRAME_WIDTH) ||
         ((y + pattern.height) > DISPLAY_FRAME_HEIGHT))
         return false
-
-    // Simple naive brute force match.
-    // TODO: See if a two-dimensional variant of the Boyer-Moore algorithm can be used instead.
 
     for (py in 0 until pattern.height) {
         for (px in 0 until pattern.width) {
@@ -69,21 +78,13 @@ fun checkIfPatternMatchesAt(displayFrame: DisplayFrame, pattern: Pattern, x: Int
 }
 
 /**
- * Look for regions in the display frame that can  be turned into tokens.
- *
- * This will first do a pattern matching search, and then try to filter out matches that
- * overlap with other matches and are considered to be unnecessary / undesirable by the
- * internal heuristic (for example, a part of the multiwave bolus pattern also looks like
- * the character L, but in case of such an overlap, we are interested in the former).
- * The remaining matches are output as tokens.
+ * Look for regions in the display frame that can be turned into tokens.
  *
  * @param displayFrame [DisplayFrame] to search for tokens.
  * @return Tokens found in this frame.
  */
 fun findTokens(displayFrame: DisplayFrame): Tokens {
     val tokens = mutableListOf<Token>()
-
-    // Scan through the display frame and look for tokens.
 
     var y = 0
 
@@ -93,14 +94,8 @@ fun findTokens(displayFrame: DisplayFrame): Tokens {
         while (x < DISPLAY_FRAME_WIDTH) {
             for ((glyph, pattern) in glyphPatterns) {
                 if (checkIfPatternMatchesAt(displayFrame, pattern, x, y)) {
-                    // Current region in the display frame matches this pattern.
-                    // Create a token out of the pattern, glyph, and coordinates,
-                    // add the token to the list of found tokens, and move past the
-                    // matched pattern horizontally. (There's no point in advancing
-                    // pixel by pixel horizontally since the next pattern.width pixels
-                    // are guaranteed to be part of the already discovered token).
                     tokens.add(Token(pattern, glyph, x, y))
-                    x += pattern.width - 1 // -1 since the x value is incremented below.
+                    x += pattern.width - 1
                     break
                 }
             }
@@ -111,24 +106,10 @@ fun findTokens(displayFrame: DisplayFrame): Tokens {
         y++
     }
 
-    // Check for overlaps. The pattern matching is not automatically unambiguous.
-    // For example, one of the corners of the multiwave bolus icon also matches
-    // the small 'L' character pattern. Try to resolve overlaps here and remove
-    // tokens if required. (In the example above, the 'L' pattern match is not
-    // needed and can be discarded - the match of interest there is the multiwave
-    // bolus icon pattern match.)
-
     val tokensToRemove = mutableSetOf<Token>()
 
-    // First, determine what tokens to remove.
     for (tokenB in tokens) {
         for (tokenA in tokens) {
-            // Get the coordinates of the top-left (x1,y1) and bottom-right (x2,y2)
-            // corners of the bounding rectangles of both matches. The (x2,y2)
-            // coordinates are inclusive, that is, still inside the rectangle, and
-            // at the rectangle's bottom right corner. (That's why there's the -1
-            // in the calculations below; it avoids a fencepost error.)
-
             val tokenAx1 = tokenA.x
             val tokenAy1 = tokenA.y
             val tokenAx2 = tokenA.x + tokenA.pattern.width - 1
@@ -139,47 +120,6 @@ fun findTokens(displayFrame: DisplayFrame): Tokens {
             val tokenBx2 = tokenB.x + tokenB.pattern.width - 1
             val tokenBy2 = tokenB.y + tokenB.pattern.height - 1
 
-            /* Overlap detection:
-
-            Given two rectangles A and B:
-
-            Example of non-overlap:
-
-            <                  A                  >
-                                                            <                  B                  >
-                                                  |---xd2---|
-
-            |-----------------------------------------xd1-----------------------------------------|
-
-            Example of overlap:
-
-            <                  A                  >
-                          <                  B                  >
-                          |----------xd2----------|
-
-            |------------------------xd1------------------------|
-
-            xd1 = distance from A.x1 to B.x2
-            xd2 = distance from A.x2 to B.x1
-
-            If B is fully to the right of A, then both xd1 and xd2 are positive.
-            If B is fully to the left of A, then both xd1 and xd2 are negative.
-            If xd1 is positive and xd2 is negative (or vice versa), then A and B are overlapping in the X direction.
-
-            The same tests are done in Y direction.
-
-            If A and B overlap in both X and Y direction, they overlap overall.
-
-            It follows that:
-            if (xd1 is positive and xd2 is negative) or (xd1 is negative and xd2 is positive) and
-               (yd1 is positive and yd2 is negative) or (yd1 is negative and yd2 is positive) -> A and B overlap.
-
-            The (xd1 is positive and xd2 is negative) or (xd1 is negative and xd2 is positive) check
-            can be shorted to: (sign(xd1) != sign(xd2)). The same applies to the checks in the Y direction.
-
-            -> Final check: if (sign(xd1) != sign(xd2)) and (sign(yd1) != sign(yd2)) -> A and B overlap.
-            */
-
             val xd1 = (tokenBx2 - tokenAx1)
             val xd2 = (tokenBx1 - tokenAx2)
             val yd1 = (tokenBy2 - tokenAy1)
@@ -188,20 +128,6 @@ fun findTokens(displayFrame: DisplayFrame): Tokens {
             val tokensOverlap = (xd1.sign != xd2.sign) && (yd1.sign != yd2.sign)
 
             if (tokensOverlap) {
-                // Heuristic for checking if one of the two overlapping tokens
-                // needs to be removed:
-                //
-                // 1. If one token has a large pattern and the other doesn't,
-                //    keep the large one and discard the smaller one. Parts of larger
-                //    patterns can be misintepreted as some of the smaller patterns,
-                //    which is the reason for this heuristic.
-                // 2. If one token has a larger numSetPixels value than the other,
-                //    pick that one. A higher number of set pixels is considered to
-                //    indicate a more "complex" or "informative" pattern. For example,
-                //    the 2 blocks of 2x2 pixels at the top ends of the large 'U'
-                //    character token can also be interpreted as a large dot token.
-                //    However, the large dot token has 4 set pixels, while the large
-                //    'U' character token has many more, so the latter "wins".
                 if (tokenA.glyph.isLarge && !tokenB.glyph.isLarge)
                     tokensToRemove.add(tokenB)
                 else if (!tokenA.glyph.isLarge && tokenB.glyph.isLarge)
@@ -214,7 +140,6 @@ fun findTokens(displayFrame: DisplayFrame): Tokens {
         }
     }
 
-    // The actual token removal.
     if (tokensToRemove.isNotEmpty())
         tokens.removeAll(tokensToRemove)
 
