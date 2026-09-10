ae02447719df68594f713b07d95b20ae80a8ab91
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-10 21:13:45 +0900

    Update build.gradle.kts

--- pump/medtronic/build.gradle.kts
+++ pump/medtronic/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.ksp)
     id("kotlin-android")
+    id("kotlin-kapt")
     id("android-module-dependencies")
     id("test-module-dependencies")
     id("jacoco-module-dependencies")
@@ -9,6 +9,11 @@ plugins {
 
 android {
     namespace = "app.aaps.pump.medtronic"
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -25,6 +30,7 @@ dependencies {
     testImplementation(project(":core:keys"))
     testImplementation(project(":shared:tests"))
 
-    ksp(libs.com.google.dagger.compiler)
-    ksp(libs.com.google.dagger.android.processor)
+    api(libs.com.google.dagger.android.support)
+    kapt(libs.com.google.dagger.compiler)
+    kapt(libs.com.google.dagger.android.processor)
 }
