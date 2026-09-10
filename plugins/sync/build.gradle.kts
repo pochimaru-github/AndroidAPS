aff2745220024157e830df29bd0596871c9f31a3
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-10 14:43:16 +0900

    Update build.gradle.kts

--- plugins/sync/build.gradle.kts
+++ plugins/sync/build.gradle.kts
@@ -5,43 +5,24 @@ plugins {
 }
 
 kotlin {
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "17"
-            }
-        }
-    }
+    // 1. 【警告への対応】Android ターゲットの明示的な登録
+    androidTarget()
 
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation(project(":core"))
+                // common の依存関係
             }
         }
         val androidMain by getting {
             dependencies {
-                implementation("androidx.appcompat:appcompat:1.6.1")
-                implementation("com.google.android.material:material:1.9.0")
+                // android の依存関係
             }
         }
     }
 }
 
-android {
-    namespace = "app.nightscout.aaps.plugins.sync"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 26
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-}
-
+// 2. 【エラーへの対応】 dependencies ブロック内に kapt を記述する
 dependencies {
     kapt("androidx.room:room-compiler:2.5.2")
 }
