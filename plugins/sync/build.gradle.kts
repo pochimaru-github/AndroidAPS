e0ff0183e902019b8dde58bd3187f83de37d8a58
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-10 10:24:23 +0900

    Update build.gradle.kts

--- plugins/sync/build.gradle.kts
+++ plugins/sync/build.gradle.kts
@@ -44,11 +44,11 @@ kapt {
 
 dependencies {
     implementation(project(":aps"))
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
 
-    kapt(libs.room.compiler)
-    implementation(libs.room.runtime)
-    implementation(libs.room.ktx)
+    kapt("androidx.room:room-compiler:2.5.0")
+    implementation("androidx.room:room-runtime:2.5.0")
+    implementation("androidx.room:room-ktx:2.5.0")
 }
