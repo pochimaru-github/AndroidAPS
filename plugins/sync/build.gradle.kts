db661e7b4a63cf9511ae6937dc5f7ca2cc361bdf
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-10 10:34:54 +0900

    Update build.gradle.kts

--- plugins/sync/build.gradle.kts
+++ plugins/sync/build.gradle.kts
@@ -43,7 +43,6 @@ kapt {
 }
 
 dependencies {
-    implementation(project(":app"))
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
