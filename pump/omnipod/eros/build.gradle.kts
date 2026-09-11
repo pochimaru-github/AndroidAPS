b76b648f40d8074893c53924f8696419f09ac799
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-11 18:44:20 +0900

    Update build.gradle.kts

--- pump/omnipod/eros/build.gradle.kts
+++ pump/omnipod/eros/build.gradle.kts
@@ -19,6 +19,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildFeatures {
         viewBinding = true
         dataBinding = true
