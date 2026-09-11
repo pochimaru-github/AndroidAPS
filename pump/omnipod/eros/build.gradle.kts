046223821358059c2766d8ce490f292a4c2b7b5e
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-11 15:33:48 +0900

    Update build.gradle.kts

--- pump/omnipod/eros/build.gradle.kts
+++ pump/omnipod/eros/build.gradle.kts
@@ -2,35 +2,18 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     id("kotlin-kapt")
+    id("android-module-dependencies")
+    id("test-module-dependencies")
+    id("jacoco-module-dependencies")
 }
 
 android {
     namespace = "app.aaps.pump.omnipod.eros"
-    compileSdk = 35
 
     defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
+        vectorDrawables.useSupportLibrary = true
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
     buildFeatures {
         dataBinding = true
     }
@@ -41,18 +24,24 @@ dependencies {
     implementation(project(":core:interfaces"))
     implementation(project(":core:keys"))
     implementation(project(":core:libraries"))
-    implementation(project(":core:ui"))
     implementation(project(":core:utils"))
+    implementation(project(":core:ui"))
     implementation(project(":core:validators"))
-
-    implementation(project(":pump:common"))
-    implementation(project(":pump:rileylink"))
     implementation(project(":pump:omnipod:common"))
+    implementation(project(":pump:common"))
 
     implementation(libs.androidx.room.runtime)
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("androidx.room:room-rxjava3:2.6.1")
     kapt(libs.androidx.room.compiler)
 
-    testImplementation(libs.org.junit.jupiter)
+    api(libs.com.google.dagger.android.support)
+    kapt(libs.com.google.dagger.compiler)
+    kapt(libs.com.google.dagger.android.processor)
+
+    androidTestImplementation(project(":shared:tests"))
+    testImplementation(libs.androidx.room.testing)
+    testImplementation(project(":implementation"))
+    testImplementation(project(":shared:impl"))
+    testImplementation(project(":shared:tests"))
 }
