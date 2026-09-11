ccd5ceeb6c967198781d8f4bde406a41b0a6acca
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-11 16:46:59 +0900

    Update build.gradle.kts

--- pump/omnipod/dash/build.gradle.kts
+++ pump/omnipod/dash/build.gradle.kts
@@ -14,6 +14,15 @@ android {
         vectorDrawables.useSupportLibrary = true
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildFeatures {
         dataBinding = true
     }
