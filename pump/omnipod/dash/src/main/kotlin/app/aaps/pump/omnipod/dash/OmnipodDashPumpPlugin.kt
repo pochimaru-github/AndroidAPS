b3cb82857068bca12c88506cccc61325145cd8c1
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-11 19:18:18 +0900

    Update OmnipodDashPumpPlugin.kt

--- pump/omnipod/dash/src/main/kotlin/app/aaps/pump/omnipod/dash/OmnipodDashPumpPlugin.kt
+++ pump/omnipod/dash/src/main/kotlin/app/aaps/pump/omnipod/dash/OmnipodDashPumpPlugin.kt
@@ -1,7 +1,6 @@
 package app.aaps.pump.omnipod.dash
 
 import android.content.Context
-import app.aaps.core.interfaces.plugin.Plugin
 import app.aaps.pump.omnipod.dash.history.database.DashHistoryDatabase
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -9,13 +8,13 @@ import javax.inject.Singleton
 @Singleton
 class OmnipodDashPumpPlugin @Inject constructor(
     private val context: Context
-) : Plugin {
+) {
 
     private val database: DashHistoryDatabase by lazy {
         DashHistoryDatabase.getInstance(context)
     }
 
-    override fun getName(): String = "Omnipod DASH"
+    fun getName(): String = "Omnipod DASH"
 
     fun isConnected(): Boolean {
         return true
