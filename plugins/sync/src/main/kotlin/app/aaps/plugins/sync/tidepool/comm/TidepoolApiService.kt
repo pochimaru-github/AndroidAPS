7a83b1e61dfe90fefcc36d017f0be6872ce1d41d
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-11 21:42:21 +0900

    Update TidepoolApiService.kt

--- plugins/sync/src/main/kotlin/app/aaps/plugins/sync/tidepool/comm/TidepoolApiService.kt
+++ plugins/sync/src/main/kotlin/app/aaps/plugins/sync/tidepool/comm/TidepoolApiService.kt
@@ -4,63 +4,25 @@ import app.aaps.plugins.sync.tidepool.messages.DatasetReplyMessage
 import app.aaps.plugins.sync.tidepool.messages.UploadReplyMessage
 import okhttp3.RequestBody
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Headers
-import retrofit2.http.POST
-import retrofit2.http.PUT
-import retrofit2.http.Path
-import retrofit2.http.Query
 
 const val SESSION_TOKEN_HEADER: String = "x-tidepool-session-token"
 
 interface TidepoolApiService {
 
-    @Headers(
-        "User-Agent: AAPS-1.0",
-        "X-Tidepool-Client-Name: aaps",
-        "X-Tidepool-Client-Version: 0.2.0"
-    )
-    @DELETE("/v1/datasets/{dataSetId}")
-    fun deleteDataSet(
-        @Header("x-tidepool-session-token") token: String,
-        @Path("dataSetId") id: String
-    ): Call<DatasetReplyMessage>
+    fun deleteDataSet(token: String, id: String): Call<DatasetReplyMessage>
 
-    @GET("/v1/users/{userId}/data_sets")
     fun getOpenDataSets(
-        @Header("x-tidepool-session-token") token: String,
-        @Path("userId") id: String,
-        @Query("client.name") clientName: String,
-        @Query("size") size: Int
+        token: String,
+        id: String,
+        clientName: String,
+        size: Int
     ): Call<List<DatasetReplyMessage>>
 
-    @GET("/v1/datasets/{dataSetId}")
-    fun getDataSet(
-        @Header("x-tidepool-session-token") token: String,
-        @Path("dataSetId") id: String
-    ): Call<DatasetReplyMessage>
+    fun getDataSet(token: String, id: String): Call<DatasetReplyMessage>
 
-    @POST("/v1/users/{userId}/data_sets")
-    fun openDataSet(
-        @Header("x-tidepool-session-token") token: String,
-        @Path("userId") id: String,
-        @Body body: RequestBody
-    ): Call<DatasetReplyMessage>
+    fun openDataSet(token: String, id: String, body: RequestBody): Call<DatasetReplyMessage>
 
-    @POST("/v1/datasets/{sessionId}/data")
-    fun doUpload(
-        @Header("x-tidepool-session-token") token: String,
-        @Path("sessionId") id: String,
-        @Body body: RequestBody
-    ): Call<UploadReplyMessage>
+    fun doUpload(token: String, id: String, body: RequestBody): Call<UploadReplyMessage>
 
-    @PUT("/v1/datasets/{sessionId}")
-    fun closeDataSet(
-        @Header("x-tidepool-session-token") token: String,
-        @Path("sessionId") id: String,
-        @Body body: RequestBody
-    ): Call<DatasetReplyMessage>
+    fun closeDataSet(token: String, id: String, body: RequestBody): Call<DatasetReplyMessage>
 }
