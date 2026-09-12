1c9dec900c92217a3e10768f5e9c369585097546
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-12 20:32:50 +0900

    Update build.gradle.kts

--- plugins/sync/build.gradle.kts
+++ plugins/sync/build.gradle.kts
@@ -1,66 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
-    kotlin("multiplatform")
+    kotlin("android")
     kotlin("kapt")
-}
-
-kotlin {
-    androidTarget()
-
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(project(":core:interfaces"))
-                implementation(project(":core:data"))
-                implementation(project(":core:objects"))
-                implementation(project(":core:keys"))
-                implementation(project(":core:ui"))
-            }
-        }
-
-        val androidMain by getting {
-            dependencies {
-                implementation(project(":core:interfaces"))
-                implementation(project(":core:data"))
-                implementation(project(":core:objects"))
-                implementation(project(":core:keys"))
-                implementation(project(":core:ui"))
-
-                implementation(libs.androidx.core)
-                implementation(libs.androidx.room.runtime)
-                implementation(libs.androidx.room)
-                implementation(libs.androidx.work.runtime)
-                
-                // Gson & Network & Socket.io (明確なGAV指定)
-                implementation(libs.com.google.code.gson)
-                implementation("com.google.android.gms:play-services-wearable:18.1.0")
-                implementation("com.squareup.okhttp3:okhttp:4.12.0")
-                implementation("com.squareup.retrofit2:retrofit:2.9.0")
-                implementation("io.socket:socket.io-client:2.0.1")
-                
-                // Dagger2
-                implementation(libs.com.google.dagger.android)
-                implementation(libs.com.google.dagger.android.support)
-            }
-        }
-    }
-}
-
-dependencies {
-    implementation(project(":core:interfaces"))
-    implementation(project(":core:data"))
-    implementation(project(":core:objects"))
-    implementation(project(":core:keys"))
-    implementation(project(":core:ui"))
-
-    implementation("com.google.android.gms:play-services-wearable:18.1.0")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("io.socket:socket.io-client:2.0.1")
-
-    add("kapt", libs.com.google.dagger.compiler)
-    add("kapt", libs.com.google.dagger.android.processor)
-    add("kapt", libs.androidx.room.compiler)
 }
 
 android {
@@ -75,4 +16,37 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(project(":core:interfaces"))
+    implementation(project(":core:data"))
+    implementation(project(":core:objects"))
+    implementation(project(":core:keys"))
+    implementation(project(":core:ui"))
+
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room)
+    implementation(libs.androidx.work.runtime)
+
+    // Gson & Network & Socket.io
+    implementation(libs.com.google.code.gson)
+    implementation("com.google.android.gms:play-services-wearable:18.1.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("io.socket:socket.io-client:2.0.1")
+
+    // Dagger2
+    implementation(libs.com.google.dagger.android)
+    implementation(libs.com.google.dagger.android.support)
+
+    // Annotation Processors (KAPT)
+    kapt(libs.com.google.dagger.compiler)
+    kapt(libs.com.google.dagger.android.processor)
+    kapt(libs.androidx.room.compiler)
 }
