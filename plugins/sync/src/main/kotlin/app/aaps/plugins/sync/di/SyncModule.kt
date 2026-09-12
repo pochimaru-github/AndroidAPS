caf018d165daeae4ef98c81f6e5a5102f4ed58e5
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-12 21:57:41 +0900

    Update SyncModule.kt

--- plugins/sync/src/main/kotlin/app/aaps/plugins/sync/di/SyncModule.kt
+++ plugins/sync/src/main/kotlin/app/aaps/plugins/sync/di/SyncModule.kt
@@ -14,19 +14,7 @@ import app.aaps.plugins.sync.nsShared.StoreDataForDbImpl
 import app.aaps.plugins.sync.nsclient.data.NSSettingsStatusImpl
 import app.aaps.plugins.sync.nsclient.data.ProcessedDeviceStatusDataImpl
 import app.aaps.plugins.sync.nsclient.services.NSClientService
-import app.aaps.plugins.sync.nsclient.workers.NSClientAddAckWorker
-import app.aaps.plugins.sync.nsclient.workers.NSClientAddUpdateWorker
-import app.aaps.plugins.sync.nsclient.workers.NSClientMbgWorker
-import app.aaps.plugins.sync.nsclient.workers.NSClientUpdateRemoveAckWorker
 import app.aaps.plugins.sync.nsclientV3.services.NSClientV3Service
-import app.aaps.plugins.sync.nsclientV3.workers.DataSyncWorker
-import app.aaps.plugins.sync.nsclientV3.workers.LoadBgWorker
-import app.aaps.plugins.sync.nsclientV3.workers.LoadDeviceStatusWorker
-import app.aaps.plugins.sync.nsclientV3.workers.LoadFoodsWorker
-import app.aaps.plugins.sync.nsclientV3.workers.LoadLastModificationWorker
-import app.aaps.plugins.sync.nsclientV3.workers.LoadProfileStoreWorker
-import app.aaps.plugins.sync.nsclientV3.workers.LoadStatusWorker
-import app.aaps.plugins.sync.nsclientV3.workers.LoadTreatmentsWorker
 import app.aaps.plugins.sync.tidepool.TidepoolFragment
 import app.aaps.plugins.sync.tidepool.auth.AuthFlowIn
 import app.aaps.plugins.sync.wear.WearFragment
@@ -36,7 +24,6 @@ import app.aaps.plugins.sync.wear.wearintegration.DataLayerListenerServiceMobile
 import app.aaps.plugins.sync.xdrip.DataSyncSelectorXdripImpl
 import app.aaps.plugins.sync.xdrip.XdripFragment
 import app.aaps.plugins.sync.xdrip.XdripPlugin
-import app.aaps.plugins.sync.xdrip.workers.XdripDataSyncWorker
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -57,24 +44,10 @@ abstract class SyncModule {
 
     @ContributesAndroidInjector abstract fun contributesNSClientService(): NSClientService
     @ContributesAndroidInjector abstract fun contributesNSClientV3Service(): NSClientV3Service
-    @ContributesAndroidInjector abstract fun contributesNSClientWorker(): NSClientAddUpdateWorker
-    @ContributesAndroidInjector abstract fun contributesNSClientAddAckWorker(): NSClientAddAckWorker
-    @ContributesAndroidInjector abstract fun contributesNSClientUpdateRemoveAckWorker(): NSClientUpdateRemoveAckWorker
-    @ContributesAndroidInjector abstract fun contributesNSClientMbgWorker(): NSClientMbgWorker
-
-    @ContributesAndroidInjector abstract fun contributesLoadStatusWorker(): LoadStatusWorker
-    @ContributesAndroidInjector abstract fun contributesLoadLastModificationWorker(): LoadLastModificationWorker
-    @ContributesAndroidInjector abstract fun contributesLoadBgWorker(): LoadBgWorker
-    @ContributesAndroidInjector abstract fun contributesLoadFoodsWorker(): LoadFoodsWorker
-    @ContributesAndroidInjector abstract fun contributesLoadProfileStoreWorker(): LoadProfileStoreWorker
-    @ContributesAndroidInjector abstract fun contributesTreatmentWorker(): LoadTreatmentsWorker
-    @ContributesAndroidInjector abstract fun contributesLoadDeviceStatusWorker(): LoadDeviceStatusWorker
-    @ContributesAndroidInjector abstract fun contributesDataSyncWorker(): DataSyncWorker
 
     @ContributesAndroidInjector abstract fun contributesTidepoolFragment(): TidepoolFragment
     @ContributesAndroidInjector abstract fun contributesAuthFlowInActivity(): AuthFlowIn
     @ContributesAndroidInjector abstract fun contributesXdripFragment(): XdripFragment
-    @ContributesAndroidInjector abstract fun contributesXdripDataSyncWorker(): XdripDataSyncWorker
     @ContributesAndroidInjector abstract fun contributesWearFragment(): WearFragment
     @ContributesAndroidInjector abstract fun contributesWearDataReceiver(): WearDataReceiver
     @ContributesAndroidInjector abstract fun contributesWatchUpdaterService(): DataLayerListenerServiceMobile
