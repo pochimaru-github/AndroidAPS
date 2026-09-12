fcce7bd2c771d02eb2280d17284f8723441db73c
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-12 19:30:44 +0900

    Update build.gradle.kts

--- pump/eopatch/core/build.gradle.kts
+++ pump/eopatch/core/build.gradle.kts
@@ -1,2 +1,24 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "app.aaps.pump.eopatch.core"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
 configurations.create("default")
 artifacts.add("default", file("libs/eopatch_core.aar"))
+
+dependencies {
+    implementation(files("libs/eopatch_core.aar"))
+}
