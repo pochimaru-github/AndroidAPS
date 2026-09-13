3bca63fc72516be2dff3ad7f7be28e4228123b42
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-13 11:45:01 +0900

    Update build.gradle.kts

--- implementation/build.gradle.kts
+++ implementation/build.gradle.kts
@@ -14,12 +14,12 @@ android {
     compileSdk = 34 // ← この1行を追加
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
