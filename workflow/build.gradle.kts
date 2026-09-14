b9edd9e33201948e6361b93e57842b81b0004b25
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-14 22:57:02 +0900

    Update build.gradle.kts

--- workflow/build.gradle.kts
+++ workflow/build.gradle.kts
@@ -9,15 +9,15 @@ plugins {
 
 android {
     namespace = "app.aaps.workflow"
-    compileSdk = 34 // ← 追加
+    compileSdk = 34
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
