bd6a01a0b4e20155f7f9756f4a69f93048fef01d
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-16 14:05:33 +0900

    Update build.gradle.kts

--- pump/combov2/comboctl/build.gradle.kts
+++ pump/combov2/comboctl/build.gradle.kts
@@ -19,16 +19,17 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
-        freeCompilerArgs = freeCompilerArgs + listOf("-Xmulti-platform", "-Xexpect-actual-classes")
     }
 
     sourceSets {
         getByName("main") {
             java.srcDirs("src/commonMain/kotlin", "src/androidMain/kotlin", "src/main/kotlin")
+            kotlin.srcDirs("src/commonMain/kotlin", "src/androidMain/kotlin", "src/main/kotlin")
             manifest.srcFile("src/androidMain/AndroidManifest.xml")
         }
         getByName("test") {
             java.srcDirs("src/commonTest/kotlin", "src/test/kotlin")
+            kotlin.srcDirs("src/commonTest/kotlin", "src/test/kotlin")
         }
     }
 }
