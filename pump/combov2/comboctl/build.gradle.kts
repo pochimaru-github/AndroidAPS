a8a6cda714223188644ae81f0e608a5aa0b35d21
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-16 12:46:57 +0900

    Update build.gradle.kts

--- pump/combov2/comboctl/build.gradle.kts
+++ pump/combov2/comboctl/build.gradle.kts
@@ -1,33 +1,18 @@
 plugins {
-    id("com.android.library")
-    kotlin("android")
+    alias(libs.plugins.android.library)
+    id("kotlin-android")
+    id("android-module-dependencies")
 }
 
 android {
     namespace = "info.nightscout.comboctl"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 26
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 
     sourceSets {
         getByName("main") {
             java.srcDirs("src/commonMain/kotlin", "src/androidMain/kotlin", "src/main/kotlin")
-            kotlin.srcDirs("src/commonMain/kotlin", "src/androidMain/kotlin", "src/main/kotlin")
         }
         getByName("test") {
             java.srcDirs("src/commonTest/kotlin", "src/test/kotlin")
-            kotlin.srcDirs("src/commonTest/kotlin", "src/test/kotlin")
         }
     }
 }
