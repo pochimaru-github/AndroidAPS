05ce7c9f7f38de93892c284881e330880a2a5f18
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-17 18:26:27 +0900

    Update ComboV2Plugin.kt

--- pump/combov2/src/main/kotlin/info/nightscout/pump/combov2/ComboV2Plugin.kt
+++ pump/combov2/src/main/kotlin/info/nightscout/pump/combov2/ComboV2Plugin.kt
@@ -106,7 +106,7 @@ import info.nightscout.comboctl.base.Logger as ComboCtlLogger
 import info.nightscout.comboctl.base.Tbr as ComboCtlTbr
 import info.nightscout.comboctl.main.Pump as ComboCtlPump
 import info.nightscout.comboctl.main.PumpManager as ComboCtlPumpManager
-import info.nightscout.comboctl.main.PumpStatus
+import info.nightscout.comboctl.base.PumpStatus
 import info.nightscout.comboctl.main.AlertScreen
 import info.nightscout.comboctl.main.CommandDescription
 
