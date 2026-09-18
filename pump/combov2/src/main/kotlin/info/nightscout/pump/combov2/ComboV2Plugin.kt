c5db3dee5fd5d7e24a6808f42b9a28ddaf00a1bf
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-18 17:27:05 +0900

    Update ComboV2Plugin.kt

--- pump/combov2/src/main/kotlin/info/nightscout/pump/combov2/ComboV2Plugin.kt
+++ pump/combov2/src/main/kotlin/info/nightscout/pump/combov2/ComboV2Plugin.kt
@@ -160,7 +160,15 @@ class ComboV2Plugin @Inject constructor(
             comment = "Not implemented (Step 1 Stub)"
         }
     }
-    
+    override fun cancelTempBasal(enforceNew: Boolean): PumpEnactResult {
+        return pumpEnactResultProvider.get().apply {
+            success = false
+            enacted = false
+            comment = "Not implemented (Step 1 Stub)"
+        }
+    }
+
+
     // Coroutine scope and the associated job. All coroutines
     // that are started in this plugin are part of this scope.
     private var pumpCoroutineScopeJob = SupervisorJob()
