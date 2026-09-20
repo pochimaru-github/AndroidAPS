2486ad89d64640a1cf53b6ceaff4656f64eefaf8
pochimaru-github <73571815+pochimaru-github@users.noreply.github.com>
2026-09-20 19:44:42 +0900

    Update ComboV2Plugin.kt

--- pump/combov2/src/main/kotlin/info/nightscout/pump/combov2/ComboV2Plugin.kt
+++ pump/combov2/src/main/kotlin/info/nightscout/pump/combov2/ComboV2Plugin.kt
@@ -87,8 +87,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import info.nightscout.comboctl.main.PumpStatus.BatteryState
-import info.nightscout.comboctl.main.PumpStatus.ReservoirState
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -157,12 +155,13 @@ class ComboV2Plugin @Inject constructor(
     override val batteryLevel: Int? = null
     override val isFakingTempsByExtendedBoluses: Boolean = false
     override val lastBolusAmount: Double? = null
+    override val lastBolusTime: Long? = null
 
-    // private val _driverStateFlow = MutableStateFlow<DriverState>(DriverState.Disconnected)
-    // val driverStateFlow: StateFlow<DriverState> = _driverStateFlow.asStateFlow()
+    private val _driverStateFlow = MutableStateFlow<DriverState>(DriverState.Disconnected)
+    val driverStateFlow: StateFlow<DriverState> = _driverStateFlow.asStateFlow()
 
-    // private val _pairedStateFlow = MutableStateFlow<Boolean>(false)
-    // val pairedStateFlow: StateFlow<Boolean> = _pairedStateFlow.asStateFlow() 
+    private val _pairedStateFlow = MutableStateFlow<Boolean>(false)
+    val pairedStateFlow: StateFlow<Boolean> = _pairedStateFlow.asStateFlow()
     
     // Coroutine scope and the associated job. All coroutines
     // that are started in this plugin are part of this scope.
@@ -213,11 +212,11 @@ class ComboV2Plugin @Inject constructor(
 
     // The current driver state. We use a StateFlow here to
     // allow other components to react to state changes.
-        private val _driverStateFlow = MutableStateFlow<DriverState>(DriverState.Disconnected)
-        val driverStateFlow: StateFlow<DriverState> = _driverStateFlow.asStateFlow()
+    // private val _driverStateFlow = MutableStateFlow<DriverState>(DriverState.Disconnected)
+    // val driverStateFlow: StateFlow<DriverState> = _driverStateFlow.asStateFlow()
 
-        private val _pairedStateFlow = MutableStateFlow<Boolean>(false)
-        val pairedStateFlow: StateFlow<Boolean> = _pairedStateFlow.asStateFlow()
+    // private val _pairedStateFlow = MutableStateFlow<Boolean>(false)
+    // val pairedStateFlow: StateFlow<Boolean> = _pairedStateFlow.asStateFlow()
 
         private val _driverStateUIFlow = MutableStateFlow<DriverState>(DriverState.Disconnected)
         val driverStateUIFlow = _driverStateUIFlow.asStateFlow()
